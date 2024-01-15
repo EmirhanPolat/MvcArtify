62ee2f9f4e95544590b37eb9e7fbe09a10c84a51
EmirhanPolat <58660645+EmirhanPolat@users.noreply.github.com>
2024-01-15 16:18:15 +0300

    FileService (upload, download, delete, listAll) implemented. SeedData commented out since we already have db entries

---
+++ MvcArtify/Controllers/FilesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcArtify.Services;
+
+namespace MvcArtify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileService _fileService;
+
+        public FilesController(FileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListAllBlobs()
+        {
+            var result = await _fileService.ListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var result = await _fileService.UploadAsync(file);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("filename")]
+        public async Task<IActionResult> Download(string fileName)
+        {
+            var result = await _fileService.DownloadAsync(fileName);
+
+            return File(result.Content, result.ContentType, result.Name);
+        }
+
+        [HttpDelete]
+        [Route("filename")]
+        public async Task<IActionResult> Delete(string fileName)
+        {
+            var result = await _fileService.DeleteAsync(fileName);
+
+            return Ok(result);
+        }
+    }
+}
--- MvcArtify/Program.cs
+++ MvcArtify/Program.cs
@@ -1,10 +1,11 @@
 using MvcArtify.DataContext;
 using Microsoft.EntityFrameworkCore;
+using MvcArtify.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-//builder.Services.AddSingleton<FileService>();
+builder.Services.AddSingleton<FileService>();
 builder.Services.AddDbContext<MvcArtifyContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MvcArtifyContext") ?? throw new InvalidOperationException("Connection string 'MvcArtifyContext' not found.")));
 
@@ -17,6 +18,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
 
+    //Only seed when no data exits in db
     //SeedData.Initialize(services);
 }
 
